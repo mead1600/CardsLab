8245c228dccc09c51e1ddc74f1504fdfbdbc45d6
BT_1N3_17 <xcgncfgbnxfhg>
2019-01-10 11:27:07 -0500

    fgnbcfgnfcfgng

--- src/Deck.java
+++ src/Deck.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 
 public class Deck {
 
-    private ArrayList<Card> unDealt;
-    private ArrayList<Card> Dealt;
+    private ArrayList<Card> unDealt = new ArrayList<>();
+    private ArrayList<Card> Dealt = new ArrayList<>();
 
     public Deck(String[] rank, String[] suit, int[] pointValue){
+        ArrayList<Card> cards = new ArrayList<>();
         int possibilities = rank.length * suit.length * pointValue.length;
         for(int i = 0; i < possibilities; i++){
-            Card addition = new Card(rank[(int)(Math.random()*rank.length)],suit[(int)(Math.random()*rank.length)],pointValue[(int)(Math.random()*rank.length)]);
+            Card addition = new Card(rank[(int)(Math.random()*rank.length - 1)],suit[(int)(Math.random()*rank.length - 1)],pointValue[(int)(Math.random()*rank.length - 1)]);
+            System.out.println(addition.toString());
             unDealt.add(addition);
         }
     }
@@ -38,5 +40,13 @@ public class Deck {
             Dealt.remove(r);
         }
     }
+
+    /*public String toString(){
+        String[] cards = new String[unDealt.size()];
+        for(int i = 0; i < unDealt.size(); i++){
+            cards[i] = ""+unDealt.get(i).getRank()+", "+unDealt.get(i).getSuit()+", "+unDealt.get(i).getPointValue()+"";
+        }
+        return cards;
+    }*/
     
 }
